2cf53243e9583c2faf25b89995551de47c03b59a
lucassec1 <lucasemmanuelcs@gmail.com>
2024-04-23 21:57:10 +0000

    :sparkles: feat: add global config and migrate

---
+++ src/nest-modules/global-config.ts
@@ -0,0 +1,16 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
