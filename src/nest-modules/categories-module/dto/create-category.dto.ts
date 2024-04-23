f230a1eb56aa181d7e3115fb0d919c6f3dd524c0
lucassec1 <lucasemmanuelcs@gmail.com>
2024-04-23 21:54:47 +0000

    :sparkles: feat: add category dto's

---
+++ src/nest-modules/categories-module/dto/create-category.dto.ts
@@ -0,0 +1,4 @@
+import { CreateCategoryInput } from 'src/core/category/application/use-cases/create-category/create-category.input';
+
+export class CreateCategoryDto extends CreateCategoryInput {}
+
---
+++ src/nest-modules/categories-module/dto/search-categories.dto.ts
@@ -0,0 +1,10 @@
+import { ListCategoriesInput } from 'src/core/category/application/use-cases/list-categories/list-categories.use-case';
+import { SortDirection } from 'src/core/shared/domain/repository/search-params';
+
+export class SearchCategoriesDto implements ListCategoriesInput {
+  per_page?: number;
+  sort?: string;
+  sort_dir: SortDirection;
+  filter?: string;
+  page?: number;
+}
