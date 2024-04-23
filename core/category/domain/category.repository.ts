1ab9c89a20b477bbdf3e40c9b74086f76039e8a0
lucassec1 <lucasemmanuelcs@gmail.com>
2024-04-23 21:48:05 +0000

    :recycle: refactor: change imports on the folders

--- core/category/domain/category.repository.ts
+++
@@ -1,5 +0,0 @@
-import { IRepository } from 'core/shared/domain/repository/repository-interface';
-import { Category, CategoryId } from './category.entity';
-
-export interface ICategoryRepository
-  extends IRepository<Category, CategoryId> {}
